e07c5e21c13eac302a7d49623c0d9b569832a834
Daxius121 <gooddaytoyou121@gmail.com>
2022-06-24 13:42:51 -0700

    Main java

---
+++ js/main.js
@@ -0,0 +1,33 @@
+class country {
+    constructor(countryname,language, capital, religion,government,currency) {
+        this.countryname = countryname
+        this.language = language
+        this.capital = capital
+        this.religion = religion
+        this.government = government
+        this.currency = currency
+    }
+}
+const USA = new country("United States of America","English","Washington, D.C.","Christianity","Constitutional Republic", "U.S. Dollar (USD)")
+const Germany = new country("Germany","German","Berlin","None","Parliamentary Republic","Euro (EUR)")
+const Japan = new country("Japan","Japanese","Tokyo","None","Parliamentary Constitutional Monarchy","Japanese Yen")
+const SouthKorea = new country("South Korea","Korean","Seoul","None","Unitary Presidiential Republic","Korean Republic Won (KRW)")
+const Italy = new country("Italy","Italian","Rome","Christianity","Unitary Parliamentary Republic","Euro (EUR)")
+const Thailand = new country("Thailand","Thai","Bangkok","Buddhism","Unitary Parliamentary Semi-Democratic Constitutional Monarchy","Baht (THB)")
+const Philippines = new country("Philippines","Filipino","Manila","Roman Catholic","Unitary Presidential Republic","Philippine Peso (PHP)")
+const Australia = new country("Australia","English","Canberra","Christianity","Federal Parliamentary Constitutional Monarchy","Australian Dollar (AUD)")
+const France = new country("France","French","Paris","Christianity","Unitary Semi-Presidential Republic","Euro (EUR)")
+const Norway = new country("Norway","Norwegian","Oslo","Christianity","Unitary Parliamentary Constitutional Monarchy","Norwegian Krone (NOK)")
+const imgItem = document.getElementById("flagimage")
+const countryList = [USA,Germany,Japan,SouthKorea,Italy,Thailand,Philippines,Australia,France,Norway]
+const flagArray = ["./../images/flagofusa.png","./../images/flagofgermany.png","./../images/flagofjapan.png","./../images/flagofsouthkorea.png","./../images/flagofitaly.png","./../images/flagofthailand.png","./../images/flagofphilippines.png","./../images/flagofaustralia.png","./../images/flagoffrance.png","./../images/flagofnorway.png"]
+
+function pickACountry() {
+    const randomIndex = Math.floor(Math.random() * countryList.length)
+    const displayCountry = countryList[randomIndex]
+    const countryFlag = flagArray[randomIndex]
+    imgItem.src = countryFlag
+    nameofcountry.innerText = displayCountry.countryname
+}
+pickACountry()
+console.log(countryList)
